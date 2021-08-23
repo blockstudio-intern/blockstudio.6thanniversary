17dc5e4b25ea9794f849f896f2eb16b7c6f44b33
LU,WEI-TZU <xjjoh099229@gmail.com>
2021-08-24 00:07:34 +0800

    Merge 01

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,50 @@ document.getElementById("coords").addEventListener('mousemove', function(event) 
     document.getElementById("a-cursor").style.left = coordimg_x + 'px';
     document.getElementById("a-cursor").style.top = coordimg_y + 'px'; 
 });
+/* Show Time */
+function showTime() {
+    var date = new Date();
+    var y = date.getFullYear();
+    var mon = 1 + date.getMonth();
+    var d = date.getDate();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var anniversary = new Date(2015, 05, 17);
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = y + "/" + mon + "/" + d + "  " + h + ":" + m + ":" + s + " " + session;
+    document.getElementById("TimeDisplay").innerText = time;
+    document.getElementById("TimeDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
+
+var $container = $("#container");
+
+function update() {
+    Draggable.create(".map", {
+        bounds: $container,
+        edgeResistance: 0.65,
+        type: "x,y",
+        throwProps: true,
+        autoScroll: true,
+    });
+}
+
+update();
