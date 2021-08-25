dd47c2d19b39b1398e2cac357b717b2c432f6222
hkt725 <kate@blockstudio.tw>
2021-08-25 16:55:23 +0800

    the-main-island-popup-1

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,6 +100,33 @@ $(function () {
         scale: [-1, -1]
     }));
 });
+// ** horizontal scroll ** \\
+$(function () { // wait for document ready
+
+    var controller = new ScrollMagic.Controller();
+
+    var horizontalSlide = new TimelineMax()
+        // animate panels
+        .to("#js-slideContainer", 1, { x: "-20%" })
+        .to("#js-slideContainer", 1, { x: "-40%" })
+        .to("#js-slideContainer", 1, { x: "-60%" })
+        .to("#js-slideContainer", 1, { x: "-80%" })
+
+
+    // create scene to pin and link animation
+    new ScrollMagic.Scene({
+        triggerElement: "#js-wrapper",
+        triggerHook: "onLeave",
+        duration: "1"
+    })
+        .setPin("#js-wrapper")
+        .setTween(horizontalSlide)
+        //.addIndicators() // add indicators (requires plugin)
+        .addTo(controller);
+
+
+
+});
 
 
 
