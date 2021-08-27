0f93c0336281467bda8dc465efa5481a5ca45307
hkt725 <kate@blockstudio.tw>
2021-08-27 11:09:51 +0800

    all

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,15 +1,57 @@
+/*
 $(function() {
     $(window).on("load", function() {
-        $(".loader").fadeOut(3800);
-        $("video").fadeIn(5000);
+        $(".loader").fadeOut(4000);
     });
 });
+*/
 
+/*
 var video = document.getElementById("video_background");
     video.addEventListener("canplay", function() {
     setTimeout(function() {
         video.play();
-        }, 200);
+        }, 3800);
         video.pause();
         
     });
+*/
+function fadeInPage() {
+    if (!window.AnimationEvent) { return; }
+    var fader = document.getElementById('fader');
+    fader.classList.add('fade-out');
+}
+fadeInPage();
+
+document.addEventListener('DOMContentLoaded', function() {
+    if (!window.AnimationEvent) { return; }
+    var anchors = document.getElementsByTagName('a');
+    for (var idx=0; idx<anchors.length; idx+=1) {
+        if (anchors[idx].hostname !== window.location.hostname ||
+            anchors[idx].pathname === window.location.pathname) {
+            continue;
+        }
+        anchors[idx].addEventListener('click', function(event) {
+            var fader = document.getElementById('fader'),
+                anchor = event.currentTarget;
+            
+            var listener = function() {
+                window.location = anchor.href;
+                fader.removeEventListener('animationend', listener);
+            };
+            fader.addEventListener('animationend', listener);
+            
+            event.preventDefault();
+            fader.classList.add('fade-in');
+        });
+    }
+});
+
+window.addEventListener('pageshow', function (event) {
+    if (!event.persisted) {
+      return;
+    }
+    var fader = document.getElementById('fader');
+    fader.classList.remove('fade-in');
+  });
+
