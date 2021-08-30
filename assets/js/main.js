50e28793338869cfd1c0ffb88802ffcf77b8b10f
hkt725 <kate@blockstudio.tw>
2021-08-30 21:39:40 +0800

    easter-egg

--- assets/js/main.js
+++ assets/js/main.js
@@ -77,78 +77,78 @@ showTime();
 
 
 // confetti //
-function createRipple(y, x) {
-  const ripple = `<div class="circle" style="top:${y}px;left:${x}px;"></div>`;
-  console.log(x);
-  const _ripple = $(ripple);
-  $('.ripple').append(_ripple);
-  setTimeout(() => _ripple.remove(), 5000);
-}
+// function createRipple(y, x) {
+//   const ripple = `<div class="circle" style="top:${y}px;left:${x}px;"></div>`;
+//   console.log(x);
+//   const _ripple = $(ripple);
+//   $('.ripple').append(_ripple);
+//   setTimeout(() => _ripple.remove(), 5000);
+// }
 
-function confetti() {
-  [{
-    bg: '#FFC700',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-  }, {
-    bg: '#BADA45',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-  }, {
-    bg: '#F0983F',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-  }, {
-    bg: '#0F4C81',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-  }, {
-    bg: '#4F9D00',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-  }, {
-    bg: 'transparent',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-    internal: '😜',
-  }, {
-    bg: 'transparent',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-    internal: '😽',
-  }, {
-    bg: 'transparent',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-    internal: '🥳',
-  }, {
-    bg: 'transparent',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-    internal: '🦦',
-  }, {
-    bg: 'transparent',
-    position: Math.random() * $('html').width(),
-    speed: Math.floor(Math.random() * 5)+1,
-    internal: '🌸',
-  }, {
-    bg: 'transparent',
-    position: Math.random() * $('html').width(),
-    height: Math.random() * $('html').height(),
-    speed: Math.floor(Math.random() * 5)+1,
-    internal: '✨',
-  }].map(options => {
-    const c = $(`<div class="confetti" style="background:${options.bg};left:${options.position}px;animation: fall ${options.speed}s cubic-bezier(0.05, 0.46, 1, 1) both;">${options.internal || ''}</div>`);
-    $('body').append(c);
-    setTimeout(() => c.remove(), 5000);
-  });
-}
+// function confetti() {
+//   [{
+//     bg: '#FFC700',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//   }, {
+//     bg: '#BADA45',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//   }, {
+//     bg: '#F0983F',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//   }, {
+//     bg: '#0F4C81',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//   }, {
+//     bg: '#4F9D00',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//   }, {
+//     bg: 'transparent',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//     internal: '😜',
+//   }, {
+//     bg: 'transparent',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//     internal: '😽',
+//   }, {
+//     bg: 'transparent',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//     internal: '🥳',
+//   }, {
+//     bg: 'transparent',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//     internal: '🦦',
+//   }, {
+//     bg: 'transparent',
+//     position: Math.random() * $('html').width(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//     internal: '🌸',
+//   }, {
+//     bg: 'transparent',
+//     position: Math.random() * $('html').width(),
+//     height: Math.random() * $('html').height(),
+//     speed: Math.floor(Math.random() * 5)+1,
+//     internal: '✨',
+//   }].map(options => {
+//     const c = $(`<div class="confetti" style="background:${options.bg};left:${options.position}px;animation: fall ${options.speed}s cubic-bezier(0.05, 0.46, 1, 1) both;">${options.internal || ''}</div>`);
+//     $('body').append(c);
+//     setTimeout(() => c.remove(), 5000);
+//   });
+// }
 
-$('.celebrate-btn').mousedown(e => {
-  const offset = $(e.target).offset();
-  createRipple(e.pageY - offset.top, e.pageX - offset.left);
-  confetti();
-});
+// $('.celebrate-btn').mousedown(e => {
+//   const offset = $(e.target).offset();
+//   createRipple(e.pageY - offset.top, e.pageX - offset.left);
+//   confetti();
+// });
 
 var obj = this;
 var timer = null;
@@ -166,19 +166,93 @@ jQuery(".celebrate-btn").on('click',function(event){
     clearTimeout(timer);
     timer=setTimeout(function(){
     console.log('点击一次执行这部分代码');
-    /*
-      function(){...}
-    */
+    function createRipple(y, x) {
+    const ripple = `<div class="circle" style="top:${y}px;left:${x}px;"></div>`;
+    console.log(x);
+    const _ripple = $(ripple);
+    $('.ripple').append(_ripple);
+    setTimeout(() => _ripple.remove(), 5000);
+    }
+
+    function confetti() {
+      [{
+        bg: '#FFC700',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+      }, {
+        bg: '#BADA45',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+      }, {
+        bg: '#F0983F',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+      }, {
+        bg: '#0F4C81',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+      }, {
+        bg: '#4F9D00',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+      }, {
+        bg: 'transparent',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+        internal: '👻',
+      }, {
+        bg: 'transparent',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+        internal: '😽',
+      }, {
+        bg: 'transparent',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+        internal: '🥳',
+      }, {
+        bg: 'transparent',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+        internal: '🦦',
+      }, {
+        bg: 'transparent',
+        position: Math.random() * $('html').width(),
+        speed: Math.floor(Math.random() * 5)+1,
+        internal: '🌸',
+      }, {
+        bg: 'transparent',
+        position: Math.random() * $('html').width(),
+        height: Math.random() * $('html').height(),
+        speed: Math.floor(Math.random() * 5)+1,
+        internal: '✨',
+      }].map(options => {
+        const c = $(`<div class="confetti" style="background:${options.bg};left:${options.position}px;animation: fall ${options.speed}s cubic-bezier(0.05, 0.46, 1, 1) both;">${options.internal || ''}</div>`);
+        $('body').append(c);
+        setTimeout(() => c.remove(), 5000);
+      });
+    }
+
+    $('.celebrate-btn').mousedown(e => {
+      const offset = $(e.target).offset();
+      createRipple(e.pageY - offset.top, e.pageX - offset.left);
+      confetti();
+    });
     },250)
   }else {
     //
     clearTimeout(timer);
     }
-  if(count>20){
+  if(count>30){
     console.log('点击4次以上执行这部分代码');
-  /*
-    function(){...}
-  */
+    const d = $(`<audio src="https://blockstudio-intern.github.io/Ben_afterparty/sound/loops_8bit.mp3" controls="" id="audioPlayer" style="display: none" autoplay="true"></audio>`);
+    $('.nav').addClass('active')
+    $('.nav').append(d);
+    setTimeout(() => d.remove(), 15000);
+    setTimeout(function() {
+    $('.nav').removeClass('active');
+    }, 15000);
+
     }
 });
 
